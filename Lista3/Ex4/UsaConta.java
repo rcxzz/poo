087d3e945e5c5e37b66e899035968cd6c039f4d1
rcxzz <168548989+rcxzz@users.noreply.github.com>
2024-05-14 07:50:35 -0300

    commit 2

--- Lista3/Ex4/UsaConta.java
+++ Lista3/Ex4/UsaConta.java
@@ -4,19 +4,19 @@ public class UsaConta {
         Scanner scanner=new Scanner(System.in);
         Conta conta1=new Conta(1000.00);
         Conta conta2=new Conta(150.00);
-        System.out.printf("Saldo da Conta1: R$ %.2f \n", conta1.getSaldo());
-        System.out.printf("Saldo da Conta2: R$ %.2f \n", conta2.getSaldo());
+        System.out.printf("Saldo da Conta1: R$ " + conta1.getSaldo() + "\n");
+        System.out.printf("Saldo da Conta2: R$ " + conta2.getSaldo() + "\n");
         System.out.print("Digite o valor de retirada da conta1: ");
         double qtdeRetirada1=scanner.nextDouble();
-        System.out.printf("Subtraindo %.2f do saldo da conta1 \n\n", qtdeRetirada1);
+        System.out.printf("Subtraindo " + qtdeRetirada1 + " do saldo da conta1. \n");
         conta1.debitar(qtdeRetirada1);
-        System.out.printf("Saldo da Conta1: R$ %.2f \n", conta1.getSaldo());
-        System.out.printf("Saldo da Conta2: R$ %.2f \n", conta2.getSaldo());
+        System.out.printf("Saldo da Conta1: R$  " + conta1.getSaldo() + "\n");
+        System.out.printf("Saldo da Conta2: R$  " + conta2.getSaldo() + "\n");
         System.out.print("Digite o valor de retirada da conta2: ");
         double qtdeRetirada2=scanner.nextDouble();
-        System.out.printf("Subtraindo %.2f do saldo da conta2 \n\n", qtdeRetirada2);
+        System.out.printf("Subtraindo " + qtdeRetirada2 + " do saldo da conta2. \n\n");
         conta2.debitar(qtdeRetirada2);
-        System.out.printf("Saldo da Conta1: R$ %.2f \n", conta1.getSaldo());
-        System.out.printf("Saldo da Conta2: R$ %.2f \n", conta2.getSaldo());
+        System.out.printf("Saldo da Conta1: R$ " + conta1.getSaldo() + "\n");
+        System.out.printf("Saldo da Conta2: R$ " + conta2.getSaldo());
     }
 }
--- Lista3/Ex5/UsaFatura.java
+++ Lista3/Ex5/UsaFatura.java
@@ -3,7 +3,7 @@ public class UsaFatura {
         Fatura fatura1=new Fatura("12345", "Teclado", 2, 35.99);
         Fatura fatura2=new Fatura("67890", "Monitor", 1, 199.99);
 
-        System.out.printf("Fatura 1: " + fatura1.getNumero() + fatura1.getDescricao() +  fatura1.getQuantidade() + " a " + fatura1.getPreco() + " = " + fatura1.getQtdeFatura());  
-        System.out.printf("Fatura 2: " + fatura2.getNumero() + fatura2.getDescricao() + fatura2.getQuantidade() + " a " + fatura2.getPreco() + " = " + fatura2.getQtdeFatura());   
+        System.out.printf("Fatura 1: " + "\n Numero: " + fatura1.getNumero() + "\n Tipo: " + fatura1.getDescricao() + "\n Quantidade: " + fatura1.getQuantidade() + " a " + fatura1.getPreco() + " = " + fatura1.getQtdeFatura());  
+        System.out.printf("\n Fatura 2: " + "\n Numero: " + fatura2.getNumero() + "\n Tipo: " + fatura2.getDescricao() + "\n Quantidade: " + fatura2.getQuantidade() + " a " + fatura2.getPreco() + " = " + fatura2.getQtdeFatura());   
     }
 }
