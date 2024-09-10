aae7f645025bb3145df6d17564a2c56a43e354d0
Eric R Sant ana <168548989+rcxzz@users.noreply.github.com>
2024-09-10 08:11:13 -0300

    Update Baralho.java

--- Lista12/Baralho.java
+++ Lista12/Baralho.java
@@ -3,22 +3,22 @@ public class Baralho{
     private int tamanho;
 
     public Baralho(){
-        cartas = new Carta[52];
-        tamanho = 0;
-        for (Naipe naipe : Naipe.values()){
-            for (Valor valor : Valor.values()){
-                cartas[tamanho++] = new Carta(naipe, valor);
+        cartas=new Carta[52];
+        tamanho=0;
+        for(Naipe naipe : Naipe.values()){
+            for(Valor valor : Valor.values()){
+                cartas[tamanho++]=new Carta(naipe, valor);
             }
         }
     }
 
     public void embaralhar(){
-        int tamanho = cartas.length;
-        for (int i = tamanho - 1; i > 0; i--){
-            int j = (int) (Math.random() * (i + 1));
-            Carta temp = cartas[i];
-            cartas[i] = cartas[j];
-            cartas[j] = temp;
+        int tamanho=cartas.length;
+        for(int i=tamanho-1;i>0;i--){
+            int j=(int)(Math.random()*(i+1));
+            Carta temp=cartas[i];
+            cartas[i]=cartas[j];
+            cartas[j]=temp;
         }
     }
 
