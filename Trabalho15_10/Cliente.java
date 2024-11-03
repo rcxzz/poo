7da0c27aa8f66f653c917d0e322e04b18b52fd64
Eric R Sant ana <168548989+rcxzz@users.noreply.github.com>
2024-11-03 16:30:31 -0300

    Cliente.java

--- Trabalho15_10/Cliente.java
+++ Trabalho15_10/Cliente.java
@@ -1,15 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Classe que representa um cliente da biblioteca
 public class Cliente {
-    private String nome; // Nome do cliente
+    private String nome; 
     private List<Emprestimo> emprestimos; // Lista de empréstimos do cliente
 
     // Construtor da classe Cliente
     public Cliente(String nome) {
-        this.nome = nome; // Inicializa o nome do cliente
-        this.emprestimos = new ArrayList<>(); // Inicializa a lista de empréstimos
+        this.nome = nome;
+        this.emprestimos = new ArrayList<>();
     }
 
     // Método getter para acessar o nome do cliente
@@ -19,11 +18,11 @@ public class Cliente {
 
     // Método para adicionar um empréstimo à lista do cliente
     public void adicionarEmprestimo(Emprestimo emprestimo) {
-        emprestimos.add(emprestimo); // Adiciona o empréstimo à lista
+        emprestimos.add(emprestimo);
     }
 
     // Método getter para acessar a lista de empréstimos
     public List<Emprestimo> getEmprestimos() {
-        return emprestimos; // Retorna a lista de empréstimos
+        return emprestimos;
     }
 }
